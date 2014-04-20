eae354c943519391b248a7c7e81f114d3800dd22
Raphael Randschau <nicolai86@me.com>
2014-04-20 23:07:57 +0200

    allow accounts to be created dynamically

--- scripts/components/account-picker.js
+++ scripts/components/account-picker.js
@@ -1,42 +1,44 @@
 define(
   ['ractive','bloodhound', 'typeahead'],
-  function( Ractive, Bloodhound ) {
+  function( Ractive, Bloodhound) {
     'use strict';
 
     var counter = 0;
+    var setupAutocompletion = function setupAutocompletion() {
+      var App = require('application');
+
+      this.datasource = new Bloodhound({
+        datumTokenizer: function(d) {
+          return _.map([d.code, d.label].concat(d.label.split(',')), $.trim);
+        },
+        queryTokenizer: Bloodhound.tokenizers.whitespace,
+        local: function() {
+          return App.accounts.toJSON();
+        }
+      });
+      this.datasource.initialize();
+
+      var selector = '[name=accountCode'+this.data.suffix+']';
+      $(selector).typeahead({ highlight: true }, {
+        displayKey: 'code',
+        source: this.datasource.ttAdapter(),
+        templates: {
+          suggestion: function(item) {
+            return item.displayName;
+          }
+        }
+      }).on('typeahead:selected', function(evt, object) {
+        this.set({ account: object.code, label: object.label });
+      }.bind(this));
+    };
+
     var AccountPicker = Ractive.extend({
         template: '<input name="accountLabel{{suffix}}" type="text" value="{{label}}">' +
           '<input name="accountCode{{suffix}}" type="text" placeholder="Identifikation" value="{{account}}"/>',
         complete: function () {
-          var App = require('application');
-
-          var datasource = new Bloodhound({
-            datumTokenizer: function(d) {
-              return _.map([d.code, d.label].concat(d.label.split(',')), $.trim);
-            },
-            queryTokenizer: Bloodhound.tokenizers.whitespace,
-            local: App.accounts.toJSON()
-          });
-          datasource.initialize();
-
-          var selector = '[name=accountCode'+this.data.suffix+']';
-          $(selector).typeahead({ highlight: true }, {
-            displayKey: 'code',
-            source: datasource.ttAdapter(),
-            templates: {
-              suggestion: function(item) {
-                return item.displayName;
-              }
-            }
-          }).on('typeahead:selected', function(evt, object) {
-            this.set({ account: object.code, label: object.label });
-          }.bind(this));
-
-          var selectedAccount = App.accounts.findWhere({ code: this.data.account });
-          if (selectedAccount !== undefined) {
-            this.set({ label: selectedAccount.get('label') });
-          }
+          setupAutocompletion.bind(this).call();
         },
+        lazy: true,
         init: function () {
           counter += 1;
           this.set({ suffix: counter });
--- scripts/views/positions/edit.js
+++ scripts/views/positions/edit.js
@@ -9,8 +9,46 @@ define([
   function( Backbone, I18n, PositionsTemplate, Ractive ) {
     'use strict';
 
+    var labelForAccount = function labelForAccount(code) {
+      var App = require('application');
+      var account = App.accounts.findWhere({ code: code });
+      if (account !== undefined) {
+        return account.get('label');
+      }
+      return '';
+    }
+
+    var attrWrapper = function( attr, obj ) {
+      return {
+        get: function() { return obj[attr] },
+        set: function( value ) { debugger; obj[attr] = value }
+      }
+    };
+    var accountLabel = function( obj ) { return attrWrapper('label', obj) };
+
+    var putAccount = function putAccount( obj, code ) {
+      var App = require( 'application' );
+      obj.code = code;
+      var account = App.accounts.findWhere({ code: obj.code });
+
+      if (account === undefined) {
+        account = App.accounts.create(obj);
+      } else {
+        account.set( obj );
+        account.save();
+      }
+    }
+
     return function(position) {
       var originalAttributes = _.clone(position.attributes);
+
+      var accountFrom = {
+        label: labelForAccount( position.get('accountCodeFrom') )
+      };
+      var accountTo = {
+        label: labelForAccount( position.get('accountCodeTo') )
+      };
+
       var ractive = new Ractive({
         template: PositionsTemplate  ,
         adapt: [ Ractive.adaptors.Backbone ],
@@ -23,9 +61,10 @@ define([
           t: I18n.t
         },
 
-        lazy: true,
-
         computed: {
+          accountLabelFrom: accountLabel(accountFrom),
+          accountLabelTo: accountLabel(accountTo),
+
           totalAmount: {
             get: function() { return (this.get('position.totalAmountCents') / 100.0).toFixed(2); },
             set: function( value ) {
@@ -40,6 +79,9 @@ define([
       ractive.on({
         save: function( event ) {
           var model = event.context.position;
+
+          putAccount(accountFrom, model.get('accountCodeFrom'));
+          putAccount(accountTo, model.get('accountCodeTo'));
 
           model.save().always(function() {
             this.fire('fiscalItem:put');
