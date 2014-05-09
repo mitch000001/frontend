b5e26187bad9926c7526aefc4aaf9932300e5d7e
Raphael Randschau <nicolai86@me.com>
2014-05-09 23:36:03 +0200

    bugfix: babysitter

--- scripts/init.js
+++ scripts/init.js
@@ -48,7 +48,7 @@ require.config({
     /* alias all marionette libs */
     'backbone.marionette': '../bower_components/backbone.marionette/lib/core/amd/backbone.marionette',
     'backbone.wreqr': '../bower_components/backbone.wreqr/lib/amd/backbone.wreqr',
-    'backbone.babysitter': '../bower_components/backbone.babysitter/lib/amd/backbone.babysitter',
+    'backbone.babysitter': '../bower_components/backbone.babysitter/lib/backbone.babysitter',
     'backbone.relational': 'vendor/backbone.relational',
     'backbone.cacheit': 'vendor/backbone.cacheit',
 
